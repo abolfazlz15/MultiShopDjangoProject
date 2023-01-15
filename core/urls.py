8f15657ecaed030f5bd32bf71c314debef62692b
Abolfazl <abolfazlzaker1234@gmail.com>
2023-01-15 04:25:49 +0330

    add home page template and show category and product in home page

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from core import views
 
 app_name = 'core'
 urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
     path('contactus', views.ContactUsView.as_view(), name='contact_us'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
+
 from core.forms import ContactUsForm
 from core.models import ContactUs
-from django.urls import reverse
+from product.models import Product, Category
+from core.models import BannerHomePage
 
 class ContactUsView(generic.FormView):
     template_name = 'core/contact_us.html'
     form_class = ContactUsForm
 
     def get_success_url(self):
-        return reverse('product:product_list')    
+        return reverse('product:product_list')
 
     def form_valid(self, form):
         clean_data = form.cleaned_data
         ContactUs.objects.create(
             name=clean_data['name'], email=self.request.user, message=clean_data['message'])
         return super().form_valid(form)
+
+
+class HomePageView(generic.TemplateView):
+    template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        products = Product.objects.filter(status=True).order_by('-id')
+        context['products'] = products[:4]
+        context['products_old'] = products.order_by('created')
+        context['categories'] = Category.objects.filter(parent=None).order_by('-id')
+        # context['banners'] = BannerHomePage.objects.all()[:4]
+        return context
