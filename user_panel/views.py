da31db866e1dd6e0a59608c58f039d3bd242c2d2
Abolfazl <abolfazlzaker.dev@gmail.com>
2023-05-25 00:56:18 +0430

    feat: add status for order & show user order in user panel

--- user_panel/views.py
+++ user_panel/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
-
+from order.models import Order
 
 class IndexPageView(generic.TemplateView):
     template_name = 'user_panel/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(IndexPageView, self).get_context_data(**kwargs)
+        all_order = Order.objects.values_list('status', flat=True)
+        context['delivered_order'] = all_order.filter(status='Delivered').count()
+        context['in_Progrssing_order'] = all_order.filter(status='In Progrssing').count()
+        context['returned_order'] = all_order.filter(status='Returned').count()
         return context
     
