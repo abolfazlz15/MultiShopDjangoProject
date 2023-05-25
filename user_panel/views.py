15032208bccf6431b45b9a3196f48298ec8d7843
Abolfazl <abolfazlzaker.dev@gmail.com>
2023-05-25 15:04:20 +0430

    fix: change sidebar and fix order query

--- user_panel/views.py
+++ user_panel/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from order.models import Order
 
-class IndexPageView(generic.TemplateView):
+
+class IndexPageView(LoginRequiredMixin ,generic.TemplateView):
     template_name = 'user_panel/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(IndexPageView, self).get_context_data(**kwargs)
-        all_order = Order.objects.values_list('status', flat=True)
+        # order status in main page
+        all_order = Order.objects.filter(user=self.request.user).values_list('status', flat=True)
         context['delivered_order'] = all_order.filter(status='Delivered').count()
         context['in_Progrssing_order'] = all_order.filter(status='In Progrssing').count()
         context['returned_order'] = all_order.filter(status='Returned').count()
