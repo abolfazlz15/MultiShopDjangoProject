3a740c517919967970cbd90056d2f4f8e52f9439
Abolfazl <abolfazlzaker.dev@gmail.com>
2023-06-15 20:12:21 +0430

    feat: add advanece pagination for favorite product list

--- user_panel/views.py
+++ user_panel/views.py
@@ -166,6 +166,7 @@ class FavoriteProductList(generic.ListView):
     model = FavoriteProduct
     template_name = 'user_panel/favorite_product_list.html'
     context_object_name = 'products'
+    paginate_by = 1
 
     def get_queryset(self):
         queryset = super().get_queryset()
